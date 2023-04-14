3145162a32330625d83e13b1cf6462c215594430
ferchii1520 <fuentesdiazmafernanda@gmail.com>
2023-04-14 11:17:30 -0600

    Versión 3 for

--- Calculos.c
+++ Calculos.c
@@ -3,14 +3,10 @@ int p(int n){
     int resultado = 1;
     int k = 0;
     
-    k = 1;
-    do{
-        resultado = resultado * k;
-        k = k + 1;
-    } while (k <= n);
-    {
-        return resultado;
+    for (k = 1; k <= n; k = k + 1){
+        resultado  = resultado * k;
     }
+    return resultado;
 }
 int pNr(int n, int r){    
     return p(n)/p(n-r);  //10! / 8! = (10 * 9) * ( 8!) / 8!       
